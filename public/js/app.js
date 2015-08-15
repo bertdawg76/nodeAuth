58af77b3b7712ae606afbc592ed7b99be845c07c
philippschulte <philipp.schulte@ymail.com>
2015-08-15 15:15:37 -0600

    Completed all views

--- public/js/app.js
+++ public/js/app.js
@@ -7,22 +7,22 @@ app.config(function($routeProvider) {
   	$routeProvider
 		.when('/', {
 			templateUrl: './views/home.html',
-		  	controller: 'homeCtrl',  
+		  	controller: 'homeCtrl' 
 		})
 	    .when('/login', {
 	  		templateUrl: './views/login.html',
-	  		controller: 'authCtrl',
+	  		controller: 'authCtrl'
 	  	})
 	    .when('/register', {
 	  		templateUrl: './views/register.html',
-	  		controller: 'authCtrl',
+	  		controller: 'authCtrl'
 	  	})
 	  	.when('/products', {
 	  		templateUrl: './views/products.html',
 	  		controller: 'productsCtrl'
-	  	});
+	  	})
 	  	.otherwise({
 	    	redirectTo: '/'
-	});
+		});
 
 });
