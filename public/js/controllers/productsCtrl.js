2a1a7723427cc1852929f848445521c522612522
philippschulte <philipp.schulte@ymail.com>
2015-08-17 23:47:09 -0600

    Added functionality to add products

--- public/js/controllers/productsCtrl.js
+++ public/js/controllers/productsCtrl.js
@@ -1,12 +1,19 @@
 'use strict';
 
-app.controller('productsCtrl', ['$scope', 'products', 'networkService', function($scope, products) {
+app.controller('productsCtrl', ['$scope', 'products', 'networkService', function($scope, products, networkService) {
 
 	$scope.products = products;
 
 	var getProducts = function() {
 		networkService.getProducts().then(function(data) {
 			$scope.products = products;
+		});
+	};
+
+	$scope.addProduct = function() {
+		networkService.addProduct($scope.product).then(function(newProduct) {
+			$scope.product = {};
+			$scope.products.push(newProduct);
 		});
 	};
 
